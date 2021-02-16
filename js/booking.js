436317c31d8b9b56256289f97966b35191a84d7f
Russell Rice <russellrice15@gmail.com>
2021-02-16 15:25:42 -0500

    Add validation to reservation form

--- js/booking.js
+++ js/booking.js
@@ -3,6 +3,8 @@ const customSelect = document.querySelector('.custom-select');
 const customSelectOptions = document.querySelectorAll('.custom-option');
 const partyWrapper = document.querySelector('.party-wrapper');
 let partyNumber = 4;
+const reservationForm = document.querySelector('.reservation__form');
+const reservationInputs = document.querySelectorAll('input');
 
 // Selection of custom select
 for (const option of customSelectOptions) {
@@ -40,21 +42,52 @@ partyWrapper.addEventListener('click', function (e) {
 			partyNumber--;
 		}
 		if (partyNumber === 1) {
-			party.value = `${partyNumber} person`;
+			party.textContent = `${partyNumber} person`;
 		} else {
-			party.value = `${partyNumber} people`;
+			party.textContent = `${partyNumber} people`;
 		}
 	} else if (e.target.classList.contains('add')) {
 		if (partyNumber < 25) {
 			partyNumber++;
 		}
-		party.value = `${partyNumber} people`;
+		party.textContent = `${partyNumber} people`;
 	}
+	party.dataset.value = partyNumber;
 });
 
 // Change the minutes number input to look like time
 document.querySelector('.minutes').addEventListener('change', function () {
 	if (this.value === '0') {
 		this.value = `00`;
+	}
+});
+
+// Form validation
+function validateInputs(inputs) {
+	for (let input of inputs) {
+		if (input.value === '') {
+			input.closest('.form-group').classList.add('error');
+		}
+	}
+	for (let input of inputs) {
+		if (input.closest('.form-group').classList.contains('error')) {
+			return true;
+		}
+	}
+	return false;
+}
+
+reservationForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	if (validateInputs(reservationInputs) === true) {
+		for (let input of reservationInputs) {
+			input.addEventListener('keydown', () => {
+				input.closest('.form-group').classList.remove('error');
+			});
+		}
+	} else {
+		for (let input of reservationInputs) {
+			input.value = '';
+		}
 	}
 });
